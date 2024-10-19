90ef12dd08879b09842c109b1a244609765c33b2
DStr <dist812@yandex.ru>
2024-10-19 21:18:52 +0300

    Add notifications

---
+++ transfer_service/notifications/kafka_consumer.py
@@ -0,0 +1,20 @@
+from kafka import KafkaConsumer
+import json
+
+
+class KafkaConsumer:
+    def __init__(self, topic):
+        self.consumer = KafkaConsumer(topic, 
+                                      bootstrap_servers=['localhost:9092'], 
+                                      auto_offset_reset='earliest',
+                                      group_id='my-group',
+                                      value_deserializer=lambda x: json.loads(x.decode('utf-8')))
+    
+    def listen(self):
+        for message in self.consumer:
+            self.send_notigication(message.value)
+
+    def send_notigication(self, data):
+        print(data)
+
+    
